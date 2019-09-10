021e16b125f7a492437e10146dd8de37ddebbc51
michaelw123 <xiaoqunw@hotmail.com>
2019-09-10 13:06:33 -0400

    Free Validation

---
+++ src/main/scala/freemonads/validation.scala
@@ -0,0 +1,56 @@
+package freemonads
+
+object validation extends App {
+  sealed trait Free[F[_], A]  {
+    def flatMap[B](f: A => Free[F, B]): Free[F, B] = this match {
+      case Return(a) => f(a)
+      case FlatMap(sub, cont) => {
+        FlatMap(sub, cont andThen (_ flatMap f))
+      }
+    }
+    def  map[B](f: A => B): Free[F, B] = flatMap(a => Return(f(a)))
+  }
+  final case class Return[F[_], A](a: A) extends Free[F, A]
+  case class FlatMap[F[_], I, A](sub: F[I], cont: I => Free[F, A]) extends Free[F, A]
+
+  implicit def liftF[F[_], A](fa: F[A]): Free[F, A] = FlatMap(fa, Return.apply)
+  sealed trait Validator[A] {
+    def validate(arg:A):A
+  }
+  case class NameValidator(name:String) extends Validator[String] {
+    def validate (name:String) = name
+  }
+  case class AgeValidator(age:Int) extends Validator[Int] {
+    def validate(age: Int) = age
+  }
+
+  val validators = new  Executor[Validator] {
+    override def exec[A](fa: Validator[A]):A = fa match {
+      case NameValidator(name) => fa.validate(name.asInstanceOf[A])
+      case AgeValidator(age) => fa.validate(age.asInstanceOf[A])
+    }
+  }
+
+  val validation = for {
+    name <- NameValidator("Joe Doe")
+    age  <- AgeValidator(18)
+  } yield save(name, age)
+
+  val x = validate(validation, validators)
+
+  def save(name:String, age:Int) = println(s"save $name at age $age")
+
+  def validate[F[_], A](prg: Free[F, A], executor: Executor[F]): A = {
+    prg match {
+      case Return(a) => a
+      case FlatMap(sub, cont) => {
+        validate(cont(executor.exec(sub)), executor)
+      }
+    }
+  }
+
+
+  sealed trait Executor[F[_]] {
+    def exec[A](fa: F[A]): A
+  }
+}
